517b63bcb49a85c51d2e6f0a37b39a4c827fe528
thart003 <thart003@ucr.edu>
2022-06-21 14:36:09 -0700

    first article

--- flask-server/server.py
+++ flask-server/server.py
@@ -4,9 +4,12 @@ app = Flask(__name__)
 
 # Members API Route
 
+
 @app.route("/members")
-def members();
+def members():
+
     return {"members": ["Member1", "Member2", "Member3"]}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
